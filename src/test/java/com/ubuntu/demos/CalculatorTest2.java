74982914df56d912f6b6ccb7c035e3c62ca48e10
karthik <karthikchidambaram86@gmail.com>
2021-08-26 07:42:29 -0700

    Revert "revert changes"
    This reverts commit cfc6135b8d79359fe9f8f7ed6176ae214e119486.

--- src/test/java/com/ubuntu/demos/CalculatorTest2.java
+++ src/test/java/com/ubuntu/demos/CalculatorTest2.java
@@ -7,7 +7,7 @@ public class CalculatorTest2 {
 	
 	@Test
 	public void testAdd() {
-		System.out.println("Calculator Test 3");
+		System.out.println("Calculator Test 2");
 		Calculator calc = new Calculator();
 		double result1 = calc.add(100, 20);
 		Assert.assertEquals(130, result1, 0);
